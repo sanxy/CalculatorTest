a16cacf632ef8609a9f4ea911aa06431e5ae1265
sanxy <yusufadefolahan@gmail.com>
2020-07-08 17:48:20 +0100

    subtract,division and multiplication method done. clean code

--- app/src/main/java/com/sanxynet/calculator/unittesting/Calculator.java
+++ app/src/main/java/com/sanxynet/calculator/unittesting/Calculator.java
@@ -5,4 +5,17 @@ public class Calculator {
     public int add(int a, int b) {
         return a+b;
     }
+
+    public int subtract(int a, int b) {
+        return a-b;
+    }
+
+    public int multiply(int a, int b) {
+        return a*b;
+    }
+
+    public int divide(int a, int b) {
+        if (b==0) throw new IllegalArgumentException("This can not be divide by Zero number");
+        return a/b;
+    }
 }
--- app/src/test/java/com/sanxynet/calculator/junit/CalculatorTest.java
+++ app/src/test/java/com/sanxynet/calculator/junit/CalculatorTest.java
@@ -2,18 +2,45 @@ package com.sanxynet.calculator.junit;
 
 import com.sanxynet.calculator.unittesting.Calculator;
 
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
+    Calculator calculator;
 
+    /* initialize before each test is done */
+    @Before
+    public void setup(){
+        calculator = new Calculator();
+    }
 
     @Test
     public void testAddingTwoPositiveNumbers(){
-        Calculator calculator = new Calculator();
-        int result = calculator.add(20, 30);
-        assertEquals(50, result);
+        assertEquals(50, calculator.add(20, 30));
+    }
+
+    @Test
+    public void testSubtractionForTwoPositiveNumbers(){
+        assertEquals(20, calculator.subtract(30, 10));
+    }
+
+    @Test
+    public void testMultiplicationForTwoPositiveNumbers(){
+        assertEquals(300, calculator.multiply(30, 10));
+    }
+
+    @Test
+    public void testDivisionByNonZeroNumber(){
+        assertEquals(3, calculator.divide(30, 10));
+    }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void testDivisionByZeroNumber(){
+        assertEquals(3, calculator.divide(30, 0));
     }
 
 }
